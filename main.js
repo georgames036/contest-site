31a1c7b70213d522af34e2fed4c2927772763fea
georgames036 <georgames0.36@gmail.com>
2025-10-27 20:08:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 async function submitCode(problemId, subId, code) {
   try {
-    const res = await fetch("https://script.google.com/macros/s/AKfycbwKAdXpJwz7ncVjCwAzj1Qz50UJO_iO_5v6F96gRA5J2Xq6rGxiZsLnmX4L_ZexqslmAA/exec", {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbwbr8X1ROvYMzr6FZjg7KhP_H_fG5sNBNTrmuzl-zHu9DXlsrRuSjUasHR7j7K-ImvuJw/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ problemId, subId, lang: "javascript", code })

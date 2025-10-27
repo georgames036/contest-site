1f1c936d0e7b314358b99b1daa2c11ada6ed871b
georgames036 <georgames0.36@gmail.com>
2025-10-27 20:15:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 async function submitCode(problemId, subId, code) {
   try {
-    const res = await fetch("https://script.google.com/macros/s/AKfycbwbr8X1ROvYMzr6FZjg7KhP_H_fG5sNBNTrmuzl-zHu9DXlsrRuSjUasHR7j7K-ImvuJw/exec", {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbwrPymcUjrVSrwqiE5oEuw9eCzB3wLRB-kJ4-qQHh8dyiD8TadoTdnSStgwE5IWqy3J0Q/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ problemId, subId, lang: "javascript", code })
     });
 
     const data = await res.json();
-    document.getElementById("result").textContent = data.result;
+    document.getElementById(`result${problemId}${subId}`).textContent = data.result;
   } catch (err) {
-    document.getElementById("result").textContent = "通信エラー: " + err.message;
+    document.getElementById(`result${problemId}${subId}`).textContent = "通信エラー: " + err.message;
   }
 }
 
-// サンプルコードの自動提出（テスト用）
-const codeExample = "window.alert(2 * Number(window.prompt('入力の指示文')));";
-submitCode("1", "A", codeExample);
+// ボタンイベント登録
+document.getElementById("submit1A").addEventListener("click", () => {
+  const code = document.getElementById("code1A").value;
+  submitCode("1", "A", code);
+});
